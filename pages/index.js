4531f7b364186acc27dc55b443c4bd789b87c6af
unknown <davidsiregar699@gmail.com>
2023-01-07 14:34:12 +0700

    fixed url mongodb

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ async function handler(req, res) {
     const data = req.body;
 
     const client = await MongoClient.connect(
-      "mongodb+srv://admin_nextjs:test123@cluster0.ntrwp.mongodb.net/meetups?retryWrites=true&w=majority"
+      "mongodb+srv://admin_nextjs:test123@nodejs.n2vas.mongodb.net/meetups?retryWrites=true&w=majority"
     );
     const db = client.db();
 
